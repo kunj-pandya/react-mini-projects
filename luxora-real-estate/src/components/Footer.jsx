1ab3edffa06587f979f87735415916e88f697df5
kunj-pandya <kunjpandya998@gmail.com>
2025-10-27 20:58:44 +0530

    add footer component

--- luxora-real-estate/src/components/Footer.jsx
+++ luxora-real-estate/src/components/Footer.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 const Footer = () => {
+
+    const location = useLocation();
+    const isHomePage = location.pathname === "/";
+
     return (
-        <div></div>
+        <>
+            <footer
+                className={isHomePage ? "homePage_footer otherPage_footer" : "otherPage_footer"}
+            >
+                <div className='container'>
+                    <h4>LUXORA</h4>
+                    <p> Luxora Real Estate-designed to deliver a seamless experience for users exploring homes, apartments, and luxury spaces.</p>
+                    <ul>
+                        <li>
+                            <Link to={'/'}>Home</Link>
+                        </li>
+                        <li>
+                            <Link to={'/terms'}>Terms and Conditions</Link>
+                        </li>
+                        <li>
+                            <Link to={'/contact'}>Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className='container'>
+                    <h4>Connect with us.</h4>
+                    <p>+91987654321</p>
+                    <p>kunjpandya998@gmail.com</p>
+                    <p>© ALl Rights Reserved By Luxora!</p>
+                </div>
+            </footer>
+        </>
     )
 }
 
