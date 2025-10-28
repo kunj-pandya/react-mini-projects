3b6eca4488ca02f4f37380aecca7a094bb543d2d
kunj-pandya <kunjpandya998@gmail.com>
2025-10-28 15:24:04 +0530

    add Topvillas subComponents to home page

--- luxora-real-estate/src/components/Home.jsx
+++ luxora-real-estate/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HeroSection from '../subComponents/HeroSection';
+import TopVillas from '../subComponents/TopVillas';
 
 const Home = () => {
     return (
         <>
             <article className='page'>
                 <HeroSection />
+                <TopVillas />
             </article>
         </>
     )
