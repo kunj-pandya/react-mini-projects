337b218fec7f262cb8d2fbdc7b7eb810a9fd13d5
kunj-pandya <kunjpandya998@gmail.com>
2025-10-28 20:43:15 +0530

    add contact us component in home page

--- luxora-real-estate/src/components/Home.jsx
+++ luxora-real-estate/src/components/Home.jsx
@@ -5,6 +5,7 @@ import Regions from '../subComponents/Regions';
 import OurSpecialities from '../subComponents/OurSpecialities';
 import Host from '../subComponents/Host';
 import About from '../subComponents/About';
+import Contact from '../subComponents/Contact';
 
 const Home = () => {
     return (
@@ -16,6 +17,7 @@ const Home = () => {
                 <OurSpecialities />
                 <Host />
                 <About />
+                <Contact />
             </article>
         </>
     )
