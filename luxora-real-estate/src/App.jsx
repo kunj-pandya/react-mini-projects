c9eeed1ce9e44c55a667e61bdf84a9ccf1fd59aa
kunj-pandya <kunjpandya998@gmail.com>
2025-10-29 17:09:08 +0530

    add path for aboutus ,teams and condtion ,conact,villas,and single villas

--- luxora-real-estate/src/App.jsx
+++ luxora-real-estate/src/App.jsx
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import AboutUs from './components/Aboutus';
+import TermsAndConditions from './components/TermsAndConditions';
+import ContactUs from './components/ContectUs';
+import Villas from './components/Villas';
+import SingleVilla from './components/SingleVilla';
 
 function App() {
 
@@ -13,6 +18,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/aboutus' element={<AboutUs />} />
+          <Route path='/terms' element={<TermsAndConditions />} />
+          <Route path='/contact' element={<ContactUs />} />
+          <Route path='/villas' element={<Villas />} />
+          <Route path='/villa/:id' element={<SingleVilla />} />
         </Routes>
         <Footer />
       </Router>
