7e77002c32a1971e43f3cd40babd318320fb610e
kunj-pandya <kunjpandya998@gmail.com>
2025-11-02 10:26:38 +0530

    add accordion app

--- accordion-faq/src/App.jsx
+++ accordion-faq/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Accordion from './component/accordion';
 
 const App = () => {
+
+  const faqs = [
+    {
+      id: 1,
+      question: "What is React?",
+      answer: "React is a JavaScript library for building UIs."
+    },
+    {
+      id: 2,
+      question: "What is JSX?",
+      answer: "JSX lets you write HTML-like syntax inside JavaScript."
+    },
+    {
+      id: 3,
+      question: "What are Hooks?",
+      answer: "Hooks let you use state and other React features without writing a class."
+    }
+  ];
+
+
   return (
-    <div >App</div>
+    <><Accordion data={faqs} /></>
   )
 }
 
